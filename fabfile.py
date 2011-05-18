6b567c4cd9b28337d9b3c114d4684d9a41077cef
James Cleveland <jamescleveland@gmail.com>
2011-05-18 13:04:45 +0100

    fabric stuff

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,30 @@
-from fabric.api import local
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
 
-def prepare_deploy():
-    local('git add -p && git commit && git push origin master')
+env.hosts = ['james@localhost']
+
+def production():
+    env.hosts = ['servername']
+    env.directory = '/var/www/apps/wire'
+    env.deploy_user = "deploy"
+    env.activate = 'source /home/%s/.virtualenvs/project/bin/activate' % env.deploy_user
+
+
+def virtualenv(command):
+    with cd(env.directory):
+        sudo(env.activate + '&&' + command, user=env.deploy_user)
+
+def git_pull()
+    'Updates the repository.'
+    with cd(env.directory):
+        sudo('git pull', user=env.deploy_user)
+
+def pip_install_req():
+    virtualenv('pip install -U -r %s/REQUIREMENTS' % env.directory))
+
+def deploy_pip():
+    local('git push')
+    git_pull()
+    pip_install_req()
+    
