f50896a3f376b07da0c84f6a14288d9312bec9ec
James Cleveland <jamescleveland@gmail.com>
2011-05-18 15:39:01 +0100

    Modded fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,11 @@ from fabric.contrib.console import confirm
 
 def production():
     env.hosts = ['james@localhost']
-    env.directory = '/var/www/apps/wire'
-    env.deploy_user = "deploy"
-    env.path = env.directory+'/virt_env/'
-    env.activate = 'source %s/bin/activate' % env.path
+    env.deploy_user = "james"
+    env.parent_directory = '/var/www/apps'
+    env.directory = '%s/wire' % env.parent_directory
+    env.virt_path = '~/.virt_env/wire'
+    env.activate = 'source %s/bin/activate' % env.virt_path
 
 def user_add():
     with settings(warn_only=True):
@@ -24,20 +25,17 @@ def virtualenv(command):
 
 def git_pull():
     'Updates the repository.'
-    with cd(env.directory):
-        sudo('git clone git://github.com/radiosilence/wire.git .', user=env.deploy_user)
+    with cd(env.parent_directory):
+        sudo('git clone git://github.com/radiosilence/wire.git wire || (cd wire && git pull origin master)', user=env.deploy_user)
 
 def make_virt():
-    with cd(env.directory):
-        sudo('virtualenv . --no-site-packages', user=env.deploy_user)
+    sudo('virtualenv %s --no-site-packages' % env.virt_path, user=env.deploy_user)
 
 def pip_install_req():
-    virtualenv('pip install -U -r %s/REQUIREMENTS' % env.directory)
+    virtualenv('pip install -U -r -q %s/REQUIREMENTS' % env.directory)
 
 def deploy_pip():
     local('git push origin master')
-    #user_add()
-    #make_dir()
     git_pull()
     make_virt()
     pip_install_req()
