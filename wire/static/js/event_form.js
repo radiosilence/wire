5e1d668ec9d386e6befb4be93698c6f40eb2e1a5
James Cleveland <jamescleveland@gmail.com>
2011-05-24 23:33:46 +0100

    added event location autocomplete

---
+++ wire/static/js/event_form.js
@@ -0,0 +1,10 @@
+$(function() {
+    opts = {
+        delay: 20,
+        geocoder_types: 'street_address,route,intersection,political,country,administrative_area_level_1,administrative_area_level_2,administrative_area_level_3,colloquial_area,locality,sublocality,neighborhood,premise,subpremise,postal_code,natural_feature,airport,park,street_number',
+        geocoder_address: true,
+        maptype: 'normal'
+    }
+    $('#location').geo_autocomplete(opts); 
+    $('#meeting_place').geo_autocomplete(opts); 
+});
