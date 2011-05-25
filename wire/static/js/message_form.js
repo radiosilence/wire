aaedc1f03a41beb00830a73f5f14185e4db7a81b
James Cleveland <jamescleveland@gmail.com>
2011-05-25 13:50:18 +0100

    Removed debug func

--- wire/static/js/message_form.js
+++ wire/static/js/message_form.js
@@ -1,6 +1,5 @@
 $(function() {
     $.getJSON('/async/address-book', function(data) {
-        console.log(data);
         $("#recipients").autocomplete(data, {
             multiple: true,
             mustMatch: false,
