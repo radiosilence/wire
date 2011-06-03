304b89b4a998570da5b508b48b3d8846bf2b74fc
James Cleveland <jamescleveland@gmail.com>
2011-06-03 22:01:24 +0100

    Some other structury stuff.

--- wire/models/timeline.py
+++
@@ -1,9 +0,0 @@
-class Timeline:
-    def __init__(self, redis=None, user=None, type='Timeline')
-        self.redis = redis
-        self.user = user
-        self.type = type
-    
-    def get_updates(self);
-        pass
-        
