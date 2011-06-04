7d900d3686fe161dafa1954224d656b067356f92
James Cleveland <jamescleveland@gmail.com>
2011-06-04 04:44:03 +0100

    Added "place of interest" to location search.

--- wire/static/js/event_form.js
+++ wire/static/js/event_form.js
@@ -1,7 +1,7 @@
 $(function() {
     opts = {
         delay: 20,
-        geocoder_types: 'street_address,route,intersection,political,country,administrative_area_level_1,administrative_area_level_2,administrative_area_level_3,colloquial_area,locality,sublocality,neighborhood,premise,subpremise,postal_code,natural_feature,airport,park,street_number',
+        geocoder_types: 'street_address,route,intersection,political,country,administrative_area_level_1,administrative_area_level_2,administrative_area_level_3,colloquial_area,locality,sublocality,neighborhood,premise,subpremise,postal_code,natural_feature,airport,park,street_number,point_of_interest',
         geocoder_address: true,
         maptype: 'normal'
     }
